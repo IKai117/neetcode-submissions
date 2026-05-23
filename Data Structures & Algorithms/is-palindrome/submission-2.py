1ed5d2eb2f24e7bd0136d399018e390786a6d9ae
_IKai_ <kaipeng0117@gmail.com>
2026-05-23 17:26:53 +0800

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        a = s.replace(' ', '').lower()
+        
+        for i in a:
+            # print(i)
+            if not i.isalnum():
+                a = a.replace(i, '')
+        # if not a[-1].isalpha():
+        #    a = a[:len(a)-1]
+        print(a)
+        if a == a[::-1]:
+            return True
+        else:
+            return False
